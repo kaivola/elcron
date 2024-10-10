fd7cb515edd142ea98502af9c82db733b6ea2a27
vkaiv <ville.kaivola@gmail.com>
2024-10-10 20:12:20 +0300

    Save spot prices to database

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use chrono::{self, Days};
+use db::Database;
 
 mod config;
+mod db;
 mod parse_xml;
-
 
 #[tokio::main]
 async fn main() {
     let config = config::Config::from_file(".env");
+    let database = init_db();
     println!("config = {:?}", config);
     let url = build_url(&config);
     println!("url = {}", url);
     let body = reqwest::get(url).await.unwrap().text().await.unwrap();
-    parse_xml::parse_price_xml(&body);
+    let prices = parse_xml::parse_price_xml(&body);
+    for price in prices {
+        match database.insert_price(&price) {
+            Err(err) => println!("Error inserting {} to database: {}", price, err),
+            _ => {}
+        }
+    }
 }
 
 fn build_url(config: &config::Config) -> String {
@@ -19,4 +27,13 @@ fn build_url(config: &config::Config) -> String {
     let start = now.format("%Y%m%d0000").to_string();
     let end = now.checked_add_days(Days::new(1)).unwrap().format("%Y%m%d2300").to_string();
     format!("https://web-api.tp.entsoe.eu/api?securityToken={}&documentType=A44&in_Domain={}&out_Domain={}&periodStart={}&periodEnd={}", config.api_key, config.area, config.area, start, end)
+}
+
+fn init_db() -> Database {
+    let database = db::Database::get_connection();
+    match database.create_schema() {
+        Ok(_) => println!("Database schema created"),
+        Err(err) => println!("Error creating database schema: {}", err),
+    }
+    database 
 }
